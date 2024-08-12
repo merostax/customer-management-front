1e69053b600b28df96618b78c30f1124731c242a
kt <k.themri@ostfalia.de>
2024-08-13 00:52:06 +0200

    add jest config and some listing

---
+++ chap0/listing/listing1.2.1.ts
@@ -0,0 +1,19 @@
+export class Cat {
+    private _name: string = '';
+  
+    constructor(name?: string) {  
+      this._name = name !== undefined ? name : '';  
+    }
+  
+    get name(): string {
+      return this._name;
+    }
+  
+    set name(name: string) {
+      this._name = name;
+    }
+  
+    toString(): string {
+      return `This cat's name is ${this._name}!`;
+    }
+  }
